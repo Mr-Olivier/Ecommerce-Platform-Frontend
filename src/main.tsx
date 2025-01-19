359b460c0b3506a86709e66e41b0b241f115187a
Olivier <iradukundaolivier2000@gmail.com>
2025-01-19 09:06:26 +0200

    Add or modify src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,18 @@ import "./index.css";
 import LoginForm from "./components/Auth/LoginForm";
 import SignUpForm from "./components/Auth/SignUpForm";
 import Customer from "./components/dashboards/Customer";
+import AdminDashboard from "./components/dashboards/admin";
+import LandingPage from "./pages/LandingPage";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginForm />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginForm />} />
         <Route path="/signup" element={<SignUpForm />} />
-        <Route path="/custdash" element={<Customer />} />
+        <Route path="/customer" element={<Customer />} />
+        <Route path="admin" element={<AdminDashboard />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
