b83e37a4b2ece47b854b6ec3ef9b147a744844e2
Olivier <iradukundaolivier2000@gmail.com>
2025-01-20 19:58:43 +0200

    Add or modify tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navy: "#001D3D", // Add your custom color
+      },
+    },
   },
   plugins: [],
 };
