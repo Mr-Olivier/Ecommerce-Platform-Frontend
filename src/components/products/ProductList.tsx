866a0dcf2c61d42010c87d978169682f5d8c0ab0
Olivier <iradukundaolivier2000@gmail.com>
2025-02-02 01:49:07 +0200

    feat(products): add product component - ProductList.tsx

---
+++ src/components/products/ProductList.tsx
@@ -0,0 +1,20 @@
+// src/components/products/ProductList.tsx
+import React from "react";
+import ProductCard from "./ProductCard";
+import { Product } from "../../types/Product";
+
+interface ProductListProps {
+  products: Product[];
+}
+
+const ProductList: React.FC<ProductListProps> = ({ products }) => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
