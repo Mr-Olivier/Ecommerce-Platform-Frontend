50f6f4c2c14eac4305cd135926fbb39d391058a0
Olivier <iradukundaolivier2000@gmail.com>
2025-02-01 20:54:45 +0200

    feat: add

---
+++ src/config/seoConfig.ts
@@ -0,0 +1,10 @@
+export const seoConfig = {
+  defaultTitle: "Your Store Name",
+  defaultDescription: "Default store description goes here",
+  defaultImage: "/default-og-image.jpg",
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://yourdomain.com",
+  twitter: {
+    handle: "@yourstorehandle",
+    cardType: "summary_large_image",
+  },
+};
